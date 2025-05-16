1c57bb1a62bddf12c84976245178984627cf7eac
DecDuck <declanahofmeyr@gmail.com>
2025-05-16 14:54:45 +1000

    feat: take video by args and add frame pacing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use cairo::{Format, ImageSurface};
 use drm::control::ClipRect;
+use libc::sleep;
 use std::{
+    env,
     ops::Sub,
     path::{Path, PathBuf},
     str::FromStr,
+    thread,
     time::{Duration, SystemTime},
 };
 use video_rs::{Decoder, Location};
@@ -14,23 +17,28 @@ mod fonts;
 use display::DrmBackend;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let mut drm = DrmBackend::open_card().unwrap();
     let (width, height) = (2008, 64);
 
-    println!("{} {}", width, height);
-
     let mut surface = ImageSurface::create(Format::ARgb32, width as i32, height as i32).unwrap();
 
-    let mut decoder = Decoder::new(Location::File(PathBuf::from_str("./everyhousebar.mp4").unwrap()))
-        .expect("failed to create decoder");
+    let mut decoder = Decoder::new(Location::File(
+        PathBuf::from_str(args.get(1).unwrap()).unwrap(),
+    ))
+    .expect("failed to create decoder");
 
     let now = SystemTime::now();
 
     for frame_r in decoder.decode_iter() {
         if let Ok((time, frame)) = frame_r {
-            let current_time = now.elapsed().unwrap();
-            let diff = time.as_secs_f64() - current_time.as_secs_f64();
-            println!("vtime: {:.2}, ctime: {:.2}", time.as_secs(), current_time.as_secs_f64());
+            let vtime = time.as_secs() as f64;
+            let ctime = now.elapsed().unwrap().as_secs_f64();
+
+            // If this ever happens, fucking call me
+            if vtime > ctime {
+                thread::sleep(Duration::from_secs_f64(vtime - ctime));
+            }
 
             let mut data = surface.data().unwrap();
 
